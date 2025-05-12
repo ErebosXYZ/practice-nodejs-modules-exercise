b72c2850ea42a2d790102877d7adf405b0dd9664
ErebosXYZ <robertmaynou@gmail.com>
2025-05-12 21:26:03 +0200

    comentaris

--- 3-move-get-average.js
+++ 3-move-get-average.js
@@ -2,8 +2,12 @@
 
 const utils = require("./mis-utilidades/utils.js");
 // const {getAvgFromArray} = utils;
+// Podem crear una constant que sigui la desestructuració de l'objecte utils i agafar només la funció que ens interessa.
 const array = [1, 2, 3];
 const array2 = [10, 20, 30]
 
 console.log(utils.getAvgFromArray(array));
 console.log(utils.getAvgFromArray(array2));
+console.log(utils)
+
+// També es pot accedir a la funció desitjada directament si utilitzem la funció en la variable utils.
