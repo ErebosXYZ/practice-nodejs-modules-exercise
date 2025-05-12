b8e544c942cb37354b836a059026c2f723659427
ErebosXYZ <robertmaynou@gmail.com>
2025-05-12 21:04:38 +0200

    done

--- 1-os-module.js
+++ 1-os-module.js
@@ -1,2 +1,9 @@
 // Leer README
 
+const os = require('os');
+
+const memoryInGb = (os.totalmem() / Math.pow(1024, 3)).toFixed(1);
+
+console.log(`Nombre del equipo: ${os.hostname()}`);
+console.log(`Edición/Versión del sistema operativo: ${os.version()}`)
+console.log(`Memoria RAM total: ${memoryInGb} GB`);
